c4c6933368db32719bcc200ee0f923b8b447e9b7
Mila <Pod4asova20@gmail.com>
2022-04-01 23:53:13 +0300

    Initial commit

---
+++ src/main/java/lesson3/Main.java
@@ -0,0 +1,35 @@
+package lesson3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] words = {"dog", "cat", "pet", "lock", "mock", "lock", "fork", "mock", "golf", "lock"};
+
+        // Task 1.a
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        System.out.println(map);
+        System.out.println("");
+
+        // subtask 1.b.
+        Set<String> uniq = new HashSet<>(Arrays.asList(words));
+        System.out.println(uniq);
+        System.out.println("");
+
+        // subtask 2
+        Phonebook pb = new Phonebook();
+
+        pb.add("Ivanov", "123");
+        pb.add("Petrov", "456");
+        pb.add("Petrov", "789");
+
+        System.out.println(pb.get("Petrov"));
+    }
+}
